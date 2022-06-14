9ffc887a2d8dc9a096036d0d40fa951c2f54f609
kakazai <40651244+start2020@users.noreply.github.com>
2022-06-14 22:30:39 +0800

    Add files via upload

---
+++ inputs.sh
@@ -0,0 +1,8 @@
+cd /root/yjx/RUN-ALL-0609/
+for((i=1;i<=1;i+=1))
+do
+   #tfpython inputs.py --Note "1"    --P 8 --City HZ --T 15 --Dataset OD15_01.npz
+   #tfpython inputs.py --Note  "3"    --P 8 --City SH --T 15 --Dataset OD15_04.npz
+   #tfpython inputs.py --Note  "2"    --P 8 --City SZ --T 15 --Dataset OD15_05.npz
+done
+pwd
